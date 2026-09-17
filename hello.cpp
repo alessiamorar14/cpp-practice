cb74b03e0de6176c7bde6087a17ef0f299c865ae
alessiamorar14 <alessia.morar14@gmail.com>
2026-09-17 16:49:54 +0300

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
 int main()
-{ 
+{
   string nume;
   cout<<"Cum te cheama?";
   cin>>nume;
   cout<<"Hello, "<<nume<<"!";
+  cout<<"Bine ai venit!";
   return 0;
 }
